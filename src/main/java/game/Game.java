a7cd0349217abd7264bd5e65b2f0467dccaa6b62
YesSeri <34012616+YesSeri@users.noreply.github.com>
2022-09-27 19:54:43 +0200

    Added dices to state of Game class.

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -3,21 +3,31 @@ package game;
 public class Game {
     private Player player_1;
     private Player player_2;
+    private Dice dice_1;
+    private Dice dice_2;
     private boolean player_1_turn = true;
     private int current_turn = 1;
 
     public Game() {
         this.player_1 = new Player();
         this.player_2 = new Player();
+        this.dice_1 = new Dice();
+        this.dice_2 = new Dice();
     }
 
 
+    private int get_sum_of_dice(int dice_value_1, int dice_value_2) {
+        return dice_value_1 + dice_value_2;
+    }
+
+    private boolean is_same_value(int dice_value_1, int dice_value_2) {
+        return dice_value_1 == dice_value_2;
+    }
+
     // add value of two dices to the player whose turn it is.
     private void play_round() {
-        Dice dice_1 = new Dice();
-        Dice dice_2 = new Dice();
-        int dice_value_1 = dice_1.throw_dice();
-        int dice_value_2 = dice_2.throw_dice();
+        int dice_value_1 = this.dice_1.throw_dice();
+        int dice_value_2 = this.dice_2.throw_dice();
         int sum = dice_value_1 + dice_value_2;
         if (player_1_turn) {
             player_1.addPoints(sum);
